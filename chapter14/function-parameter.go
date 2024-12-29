1ab9d2e0e69241b538204324ee67b8bb0eab7cfc
warsland <57026973+warsland@users.noreply.github.com>
2024-12-29 20:54:10 +0800

    Update function-parameter.go

--- chapter14/function-parameter.go
+++ chapter14/function-parameter.go
@@ -1,29 +1,51 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
 
-type kelvin float64
+type fahrenheit float64
+type celsius float64
 
-//type sensor func() kelvin //使用type关键字声明函数类型
-
-func measureTemperature(samples int, sensor func() kelvin) { //measureTemperature接受另一个函数作为其第二个形参
-	//func measureTemperature(samples int, s sensor) //使用type关键字声明函数类型给后的代码声明
-	for i := 0; i < samples; i++ {
-		k := sensor()
-		fmt.Printf("%v° K\n", k)
-		time.Sleep(time.Second)
-
-	}
+func (c celsius) fahrenheit() fahrenheit {
+	return fahrenheit((c * 9.0 / 5.0) + 32.0)
 }
 
-func fakeSensor() kelvin {
-	return kelvin(rand.Intn(151) + 150)
+func (f fahrenheit) celsius() celsius {
+	return celsius((f - 32.0) * 5.0 / 9.0)
+}
+
+var line = func() {
+	fmt.Println("=======================")
+}
+
+func celsiusToFahrenheit(degree float64) (string, string) {
+	celsius := celsius(degree)
+	fahrenheit := celsius.fahrenheit()
+	celsius_1 := fmt.Sprintf("%.1f", celsius)
+	fahrenheit_1 := fmt.Sprintf("%1.f", fahrenheit)
+	return celsius_1, fahrenheit_1
+}
+
+func fahrenheitToCelsius(degree float64) (string, string) {
+	fahrenheit := fahrenheit(degree)
+	celsius := fahrenheit.celsius()
+	fahrenheit_1 := fmt.Sprintf("%.1f", fahrenheit)
+	celsius_1 := fmt.Sprintf("%.1f", celsius)
+	return fahrenheit_1, celsius_1
+}
+
+func drawTable(hdr1, hdr2 string, degree float64, getRow func(degree float64) (string, string)) {
+	line()
+	fmt.Printf("| %-8s | %-8s |\n", hdr1, hdr2)
+	for i := -40.0; i <= degree; i += 5.0 {
+		line()
+		number_string1, number_string2 := getRow(i)
+		fmt.Printf("| %-8s | %-8s |\n", number_string1, number_string2)
+	}
+	line()
 }
 
 func main() {
-	measureTemperature(3, fakeSensor) //将函数明传递给另一个函数
+	drawTable("℃", "℉", 100, celsiusToFahrenheit)
+	fmt.Println()
+	drawTable("℉", "℃", 100, fahrenheitToCelsius)
 }
